194ef92e04d715c4feaa06132c739f62fd0dbf45
Harsh R Jain <rjainharsh@gmail.com>
2021-09-06 08:48:47 +0530

    ADDED: ability to read the input choice of user

--- src/tictactoegame/TicTacToeGame.java
+++ src/tictactoegame/TicTacToeGame.java
@@ -1,8 +1,12 @@
 package tictactoegame;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
 
 	private static char board[] = new char[10];
+	private static char userInput, computerInput;
+	private static Scanner scan = new Scanner(System.in);
 	
 	private static void createBoard() {
 		for(int index=1; index<10; index++) {
@@ -10,9 +14,27 @@ public class TicTacToeGame {
 		}
 	}
 	
+	private static void readInputChoice() {
+		
+		System.out.println("Enter input choice: \n1. X \n2. O \nENTER YOUR CHOICE: ");
+		int choice = scan.nextInt();
+		
+		if(choice==1) {
+			userInput='X';
+			computerInput = 'O';
+		}
+		else {
+			userInput='O';
+			computerInput = 'X';
+		}
+		
+	}
+	
 	public static void main(String args[]) {
 		
 		createBoard();
+		readInputChoice();
+		scan.close();
 		
 	}
 	
