5cb4a7c2e9fc1bbdc3d3a139b154c182cce2eea5
Harsh R Jain <rjainharsh@gmail.com>
2021-09-06 08:20:23 +0530

    ADDED: board variable to store empty spaces

---
+++ src/tictactoegame/TicTacToeGame.java
@@ -0,0 +1,15 @@
+package tictactoegame;
+
+public class TicTacToeGame {
+
+	private static char board[] = new char[11];
+	
+	public static void main() {
+		
+		for(int i=1; i<11; i++) {
+			board[i] = ' ';
+		}
+		
+	}
+	
+}
